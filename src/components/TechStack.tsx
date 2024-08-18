006ec20cbd26e8392849225bea44ef467be10878
Kai Feinberg <kaiwfeinberg@gmail.com>
2024-08-18 10:30:27 -0500

    floating dock

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,71 +1,84 @@
 import { SiReact, SiTypescript, SiPython, SiReactquery, SiRust, SiShadcnui, SiSolidity, SiNumpy, SiTailwindcss, SiNextdotjs, SiNodedotjs, SiNodedotjsHex, SiExpo, SiExpoHex } from '@icons-pack/react-simple-icons';
 import { SiReactHex, SiTypescriptHex, SiPythonHex, SiReactqueryHex, SiRustHex, SiShadcnuiHex, SiSolidityHex, SiNumpyHex, SiTailwindcssHex, SiNextdotjsHex } from '@icons-pack/react-simple-icons';
+import { FloatingDock } from './ui/floating-dock';
 
 export default function TechStack() {
+  const links = [
+    {
+      title: "React",
+      icon: (
+        <SiReact size={32} color={SiReactHex} />
+      ),
+      href: "",
+    },
+    {
+      title: "TypeScript",
+      icon: (
+        <SiTypescript size={32} color={SiTypescriptHex} />
+      ),
+      href: "",
+    },
+    {
+      title: "Shadcn-ui",
+      icon: (
+        <SiShadcnui size={32} color={SiShadcnuiHex} />
+      ),
+      href: "",
+    },
+
+    {
+      title: "Tailwind",
+      icon: (
+        <SiTailwindcss size={32} color={SiTailwindcssHex} />
+      ),
+      href: "",
+    },
+    {
+      title: "Python",
+      icon: (
+        <SiPython size={32} color={SiPythonHex} />
+      ),
+      href: "",
+    },
+    {
+      title: "Solidity",
+      icon: (
+        <SiSolidity size={32} color={SiSolidityHex} />
+      ),
+      href: "",
+    },
+  ];
   return (
-
-    <div className='mb-10'>
-      <section className="mt-2">
-        <div className="flex flex-row items-center bg-white shadow-md rounded-lg p-8 ">
-          <h2 className="text-2xl font-semibold mb-0 mr-16 flex items-center dark:text-black">I'm most <br /> proficient in...</h2>
-          <div className="flex flex-wrap justify-center gap-4">
-            <a className="react">
-              <div className="rounded-lg p-4 border-gray-100 shadow-sm" >
-                <SiReact size={42} color={SiReactHex} />
-              </div>
-            </a>
-            <a className="typescript">
-              <div className="rounded-lg p-4 border-gray-100 shadow-sm" >
-                <SiTypescript size={42} color={SiTypescriptHex} />
-              </div>
-            </a>
-            <a className="shadcn">
-              <div className="rounded-lg p-4 border-gray-100 shadow-sm">
-                <SiShadcnui size={42} color={SiShadcnuiHex} />
-              </div>
-            </a>
-            <a className="shadcn">
-              <div className="rounded-lg p-4 border-gray-100 shadow-sm">
-                <SiTailwindcss size={42} color={SiTailwindcssHex} />
-              </div>
-            </a>
-            <a className="python">
-              <div className="rounded-lg p-4 border-gray-100 shadow-sm">
-                <SiPython size={42} color={SiPythonHex} />
-              </div>
-            </a>
-            <a className="solidity">
-              <div className="rounded-lg p-4 border-gray-100 shadow-sm" >
-                <SiSolidity size={42} color={SiSolidityHex} />
-              </div>
-            </a>
-          </div>
-        </div>
-      </section>
-
-      <div className="flex flex-row items-center bg-white shadow-md rounded-lg p-8 mt-2">
-      <h2 className="text-2xl font-semibold mb-0 mr-16 flex items-center dark:text-black">And I have <br /> experience in...</h2>
-      <div className="flex flex-wrap justify-center gap-4">
-          <div data-tip="React Query">
-            <SiReactquery size={32} color={SiReactqueryHex} />
-          </div>
-          <div data-tip="Next.js">
-            <SiNextdotjs size={32} color={SiNextdotjsHex} />
-          </div>
-          <div data-tip="node.js">
-            <SiNodedotjs size={32} color={SiNodedotjsHex} />
-          </div>
-          <div data-tip="expo">
-            <SiExpo size={32} color={SiExpoHex} />
-          </div>
-          <div data-tip="Rust">
-            <SiRust size={32} color={SiRustHex} />
-          </div>
-          <div data-tip="NumPy">
-            <SiNumpy size={32} color={SiNumpyHex} />
-          </div>
-        </div>
+    <div>
+      <div className="flex items-center justify-center w-full">
+        <FloatingDock
+          items={links}
+        />
       </div>
+       <div className="flex flex-row items-center bg-white shadow-md rounded-lg p-8 mt-2">
+       <h2 className="text-2xl font-semibold mb-0 mr-16 flex items-center dark:text-black">And I have <br /> experience in...</h2>
+       <div className="flex flex-wrap justify-center gap-4">
+           <div data-tip="React Query">
+             <SiReactquery size={32} color={SiReactqueryHex} />
+           </div>
+           <div data-tip="Next.js">
+             <SiNextdotjs size={32} color={SiNextdotjsHex} />
+           </div>
+           <div data-tip="node.js">
+             <SiNodedotjs size={32} color={SiNodedotjsHex} />
+           </div>
+           <div data-tip="expo">
+             <SiExpo size={32} color={SiExpoHex} />
+           </div>
+           <div data-tip="Rust">
+             <SiRust size={32} color={SiRustHex} />
+           </div>
+           <div data-tip="NumPy">
+             <SiNumpy size={32} color={SiNumpyHex} />
+           </div>
+         </div>
+       </div>
     </div>
+
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { SiX, SiGithub, SiLinkedin, SiFarcaster } from "@icons-pack/react-simple-icons"
 import { TextGenerateEffect } from "./ui/text-generate-effect"
+import { Badge } from "./ui/badge"
 
 export default function Hero() {
   return (
@@ -10,13 +11,16 @@ export default function Hero() {
         <section className="max-w-[1100px] mx-auto py-10 mb-10">
           <div className="container flex items-start justify-center gap-6 px-4 md:px-6">
             <div className="flex justify-end pr-10">
-              <img
-                src="/headshot.webp"
-                width="384"
-                height="480"
-                alt="Hero"
-                className="overflow-hidden rounded-xl object-cover max-w-[384px] h-[29rem]" // Fixed width
-              />
+              <div className="flex flex-col items-center">
+                <img
+                  src="/headshot.webp"
+                  width="384"
+                  height="480"
+                  alt="Hero"
+                  className="overflow-hidden rounded-xl object-cover max-w-[384px] h-[29rem]" // Fixed width
+                />
+                <p className="text-lg font-semibold mt-4"> Northwestern 25'</p>
+              </div>
             </div>
             <div className="max-w-[50%]">
               <p className="text-2xl font-bold text-muted-foreground">Hey there! I'm Kai.</p>
@@ -26,33 +30,36 @@ export default function Hero() {
                 I'm a developer, engineer, and designer passionate about building products and solving problems.
               </p>
 
+              <Badge className="text-md mt-4">🥇  $7k hackathon winnings</Badge>
+
               <div className='flex flex-row gap-4 mt-10'>
                 <a href="https://github.com/kai-feinberg?tab=repositories" target="_blank" rel="noopener noreferrer">
                   <Button variant="ghost" size="icon">
-                    <SiGithub size={32}/>
+                    <SiGithub size={32} />
                   </Button>
                 </a>
                 <a href="https://warpcast.com/kaimeariver" target="_blank" rel="noopener noreferrer">
                   <Button variant="ghost" size="icon">
-                    <SiFarcaster size={32} color="#855DCD"/>
+                    <SiFarcaster size={32} color="#855DCD" />
                   </Button>
                 </a>
                 <a href="https://www.linkedin.com/in/kai-feinberg/" target="_blank" rel="noopener noreferrer">
                   <Button variant="ghost" size="icon">
-                    <SiLinkedin size={32} color="#0A66C2"/>
+                    <SiLinkedin size={32} color="#0A66C2" />
                   </Button>
                 </a>
                 <a href="https://x.com/kai_rypto25" target="_blank" rel="noopener noreferrer">
                   <Button variant="ghost" size="icon">
-                    <SiX  size={32} />
+                    <SiX size={32} />
                   </Button>
                 </a>
               </div>
             </div>
+
           </div>
-        
-      </section>
-    </main>
+
+        </section>
+      </main>
     </div >
   )
 }
